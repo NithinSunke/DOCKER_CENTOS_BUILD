b5d1b4eb9c94c7917d6789ce9905253a9db0eef9
Sunke Nithin <sunkentihin@gmail.com>
2024-01-20 20:36:48 +0530

    gbcfx

--- docker_install.sh
+++ docker_install.sh
@@ -4,9 +4,10 @@ yum-config-manager  --add-repo https://download.docker.com/linux/centos/docker-c
 sudo yum repolist
 sudo yum install  docker-ce btrfs-progs btrfs-progs-devel -y
 sudo sh /tmp/DOCKER_CENTOS_BUILD/prepare_disks.sh 
-sudo docker-storage-config -s btrfs -d /dev/sdb1
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 sudo echo "alias docker=\"sudo /usr/bin/docker\"" >> /home/nsunke/.bash_profile
 docker ps
 systemctl restart docker.service
+#cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service.orig
+#sed -i 's/old_string/new_line/' /lib/systemd/system/docker.service
