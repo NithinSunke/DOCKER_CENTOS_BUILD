cf0c197657435ecfd777460d7bcae8f5ef37ffcd
Sunke Nithin <sunkentihin@gmail.com>
2024-01-20 20:13:57 +0530

    dsfaf

--- docker_install.sh
+++ docker_install.sh
@@ -3,7 +3,7 @@
 yum-config-manager  --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum repolist
 sudo yum install  docker-ce btrfs-progs btrfs-progs-devel -y
-sudo /tmp/DOCKER_CENTOS_BUILD/prepare_disk.sh 
+sudo /tmp/DOCKER_CENTOS_BUILD/prepare_disks.sh 
 sudo docker-storage-config -s btrfs -d /dev/sdb1
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
