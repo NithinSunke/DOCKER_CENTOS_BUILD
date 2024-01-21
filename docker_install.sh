2c459661ac50c8eb80a2f9cb8eb23cd1cfc9b62b
Sunke Nithin <sunkentihin@gmail.com>
2024-01-21 07:14:57 +0530

    asdf

--- docker_install.sh
+++ docker_install.sh
@@ -3,9 +3,6 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 sudo yum repolist
 sudo yum install docker-ce btrfs-progs btrfs-progs-devel -y
 sudo /tmp/DOCKER_CENTOS_BUILD/prepare_disk.sh 
-sudo docker-storage-config -s btrfs -d /dev/sdb1
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
 sudo echo "alias docker=\"sudo /usr/bin/docker\"" >> /home/nsunke/.bash_profile
 docker ps
 mkfs.btrfs -f -L docker1 /dev/mapper/dockerfs1
@@ -14,5 +11,7 @@ rm -Rf /var/lib/docker
 mkdir /var/lib/docker
 echo "LABEL=docker1  /var/lib/docker btrfs defaults 0 0" >> /etc/fstab
 mount /var/lib/docker
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
 systemctl enable docker.service
 systemctl start docker.service
