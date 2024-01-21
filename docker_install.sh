84ecfb7ba71236ddb9bf46daf49f70b1f6aba4ff
Sunke Nithin <sunkentihin@gmail.com>
2024-01-21 07:07:24 +0530

    fcdwsa

--- docker_install.sh
+++ docker_install.sh
@@ -8,3 +8,11 @@ sudo systemctl enable docker.service
 sudo systemctl start docker.service
 sudo echo "alias docker=\"sudo /usr/bin/docker\"" >> /home/nsunke/.bash_profile
 docker ps
+mkfs.btrfs -f -L docker1 /dev/sdb1
+systemctl stop docker.service
+rm -Rf /var/lib/docker
+mkdir /var/lib/docker
+echo "LABEL=docker1  /var/lib/docker btrfs defaults 0 0" >> /etc/fstab
+mount /var/lib/docker
+systemctl enable docker.service
+systemctl start docker.service
